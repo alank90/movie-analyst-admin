3c5d19ef1e48274ae3f8942d490c5943099d392d
Alan K <akillian@outlook.com>
2017-11-01 13:55:03 -0400

    commit

---
+++ public/lib/main.js
@@ -0,0 +1,7 @@
+// Main Javascript File
+$(document).ready(function() {
+    $("#update_button").on("click", function() {
+        console.log("You clicked me.");
+    });
+
+}); // end $(document).ready()
