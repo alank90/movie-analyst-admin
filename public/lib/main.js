13dc9d528372be42eb84fcc9698c6b721456f595
Alan K <akillian@outlook.com>
2017-11-03 13:53:25 -0400

    commit

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,15 +1,18 @@
-// Main Javascript File
+// main.js
+
+// ================ Main ============================================ 
 $(document).ready(function () {
-    $("#update_button").on("click", function(e) {
+    $("#update_button").on("click", function (e) {
         e.preventDefault();
-        
+        var updatedDocument = {};
+
         // Let's find the data-id of the checked panel
         // We will iterate thru each panel DOM object and check 
         // if its radiobox is checked, and if so we will go 
         // and grab the data-id value of this panel object
-        $(".panel").each(function( index ) {
+        $(".panel").each(function (index) {
             if ($(this).find("input[name = 'update']").is(":checked")) {
-                const updatedDocument = {
+                    updatedDocument = {
                     documentID: $(this).data("id"),
                     name: $(this).find("input#name").val(),
                     release: $(this).find("input#release").val(),
@@ -19,13 +22,31 @@ $(document).ready(function () {
                 };
                 JSON.stringify(updatedDocument);
                 console.log(Object.values(updatedDocument));
-            }
+            } // end if
         }); // .each
 
         // Now we have the document stored in JSON object, so lets form 
         // an AJAX req and grab the updated data from our document and send
         // a PUT to our API endpoint
+        $.ajax({
+            error: function(xhr, status, err) {
+                console.log("Error from AJAX call: " + err);
+            },
+            type: 'PUT',
+            data: updatedDocument,
+            url: 'localhost:8080/movies/updatemovie/' + updatedDocument.documentID,
+            dataType: 'JSON'
+        }).done(function (response) {
+            console.log("Im in AJAX call!");
+            // Check for successful (blank) response
+            if (response.msg === '') {
+                // do nothing
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
 
+        }); // end .done
 
     });  // #update_button
 
