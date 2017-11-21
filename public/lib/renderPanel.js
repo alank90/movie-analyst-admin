f9707f1d5c1a6ee6dc6fba7a0564cedd157aa4dc
Alan K <akillian@outlook.com>
2017-11-21 13:45:16 -0500

    commit

--- public/lib/renderPanel.js
+++ public/lib/renderPanel.js
@@ -21,18 +21,13 @@ const addMovie = function(movie) {
         moviePanel += movie[0].title;
         moviePanel += "</h3><div class='radio'><input type='radio' name='select'><label id='select'>Select</label></div></div><!-- panel-heading --><div class='panel-body text-center'><label for='name'>Name</label><input id='name' type='text' class='form-control' value='";
         moviePanel += movie[0].title + "'>";
-        moviePanel += "<label for='release'>Release</label><input id='release' type='text' value='";
-        moviePanel += movie[0].release + "'>";
-        moviePanel += "class='form-control'><label for='score'>Score</label><input id='score' type='text' value='";
-        moviePanel += movie[0].score + "'>";
-        moviePanel += "class='form-control'> <label for='reviewer'>Reviewer</label><input id='reviewer' type='text' value='";
-        moviePanel += movie[0].reviewer + "'>";
-        moviePanel += "class='form-control'><label for='publication'>Publication</label><input id='publication' type='text' value='";
-        moviePanel += movie[0].publication + "'>";
-        moviePanel += "class='form-control'></div></div></div>";
+        moviePanel += "<label for='release'>Release</label> <input id='release' type='text' value='" + movie[0].release + "' class='form-control'>";
+        moviePanel += "<label for='score'>Score</label> <input id='score' type='text' value='" + movie[0].score + "' class='form-control'>";
+        moviePanel += "<label for='reviewer'>Reviewer</label> <input id='reviewer' type='text' value='" + movie[0].reviewer + "' class='form-control'>";
+        moviePanel += "<label for='publication'>Publication</label> <input id='publication' type='text' value='" + movie[0].publication + "' class='form-control'>";
+        moviePanel += "</div></div></div>";
     }
     test = $("#movie_panels");
     console.log(test);
     $("#movie_panels").append(moviePanel);
 };
-
